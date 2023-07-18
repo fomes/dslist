ffa8f835f275a4b966d57cc9b163966dfe3a871a
fomes <fgomesdeluna@gmail.com>
2023-07-18 04:33:46 -0300

    GET game by id

--- src/main/java/com/fomes/dslist/services/GameService.java
+++ src/main/java/com/fomes/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.fomes.dslist.dto.GameDTO;
 import com.fomes.dslist.dto.GameMinDTO;
 import com.fomes.dslist.entities.Game;
 import com.fomes.dslist.repositories.GameRepository;
@@ -15,10 +17,16 @@ public class GameService {
   @Autowired
   private GameRepository gameRepository;
 
+  @Transactional
   public List<GameMinDTO> findAll() {
     List<Game> result = gameRepository.findAll();
     List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
     return dto;
   }
 
+  @Transactional(readOnly = true)
+  public GameDTO finById(Long id) {
+    Game result = gameRepository.findById(id).get();
+    return new GameDTO(result);
+  }
 }
